0ccd4c37b4004ba41f38d2cf4719ec7964c2d6b4
deadpatron <deadpatron@gmail.com>
2018-06-20 03:39:57 +0300

    почти готово

--- js/pictures.js
+++ js/pictures.js
@@ -65,3 +65,40 @@ var createPosts = function (postsValue) {
   return posts;
 };
 createPosts(25);
+
+var similarListElemnt = document.querySelector('.pictures');
+var similarPictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture__link');
+
+var renderPicture = function (picture) {
+  var pictureElement = similarPictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('img').src = picture.url;
+  pictureElement.querySelector('.picture__stat--likes').textContent = picture.likes;
+  pictureElement.querySelector('.picture__stat--comments').textContent = picture.comments.length;
+
+  return pictureElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < posts.length; i++) {
+  fragment.appendChild(renderPicture(posts[i]));
+}
+similarListElemnt.appendChild(fragment);
+
+var renderBigPicture = document.querySelector('.big-picture');
+renderBigPicture.classList.remove('hidden');
+renderBigPicture.querySelector('.big-picture__img').src = posts[0].url;
+renderBigPicture.querySelector('.likes-count').textContent = posts[0].likes;
+renderBigPicture.querySelector('.comments-count').textContent = posts[0].comments.length;
+// на двух почему-то не работает querySelectorAll
+renderBigPicture.querySelector('.social__comments').querySelector('img').src =
+  'img/avatar-' + Math.ceil(Math.random() * 5) + '.svg';
+renderBigPicture.querySelector('.social__comments').querySelector('.social__text').textContent =
+  posts[0].comments.join('');
+renderBigPicture.querySelector('.social__caption').textContent = posts[0].description;
+// Немогу понять почему не прячется
+var userView = document.querySelector('.social__comment-count', '.social__comment-count');
+userView.classList.add('visually-hidden');
+
